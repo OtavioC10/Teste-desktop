af0d7b32d94711a44fc3e163e56493554ee0a11b
OtavioC10 <ocustodio103@gmaIl.com>
2026-05-19 19:35:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ idade = int(input("Digite a sua idade: "))
 if idade >= 18:
     print(f"{nome}, você é maior de idade.")
 else:    print(f"{nome}, você é menor de idade.")
+
+def calcular_media(nota1, nota2):
+    media = (nota1 + nota2) / 2
+    return media   
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+media = calcular_media(nota1, nota2)
+print(f"A média das notas é: {media}")
